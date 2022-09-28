0d34bca91d45f141e9ae52acc8a12abfc1fb701a
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-09-27 21:04:29 -0400

    Added classwork

---
+++ classwork.py
@@ -0,0 +1,33 @@
+# Use a loop to write out the even numbers up to 24
+
+
+# Use a loop to write the multiples of 12 up to 100
+
+
+# Make an iterable of at least three of your classmates as strings
+
+
+# Use a loop to print out each of your classmates
+
+
+# Use a loop to print out each of your classmates and their position in the iterable
+
+
+# Have the user enter a positive integer and save it to a variable
+
+
+# Use a loop to print the number, triple it, and repeat until it hits 100,000
+
+
+# Create a blank modifiable iterable (list, set) to use as a grocery list
+
+
+# Use a loop to have the user enter items into the grocery list until they enter a blank
+
+
+# Use a loop to print back the grocery list, but select an item in advance at which
+# you stop printing the grocery list completely
+
+
+# Use a loop to print back the grocery list, but select another item in advance
+# to skip over when printing the list, but continuing to print everything else
